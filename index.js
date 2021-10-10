be48cee72888a188d56c14126f30fb3f743dd12c
Waelahmed99 <Waelahmed6599@gmail.com>
2021-10-10 17:35:45 +0200

    changing app.use position

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const app = require("express")();
-require("./startup/routes")(app);
-require("./startup/db")();
-require("./startup/prod")(app);
-
 // Adding cors to header
 const cors = require('cors')
 app.use(cors());
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("listening on ", port));
