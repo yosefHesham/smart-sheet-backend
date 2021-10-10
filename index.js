1472737450a406143770f80a76fb6dd0a7b8f59a
Waelahmed99 <Waelahmed6599@gmail.com>
2021-10-10 17:30:07 +0200

    Installing & applying cors using npm

--- index.js
+++ index.js
@@ -3,17 +3,9 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/prod")(app);
 
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   if ('OPTIONS' == req.method) {
-      res.sendStatus(200);
-    }
-    else {
-      next();
-    }
-});
+// Adding cors to header
+const cors = require('cors')
+app.use(cors());
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("listening on ", port));
