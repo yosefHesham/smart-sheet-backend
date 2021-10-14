e8aa675140eca8e1389ad152203bba76db76c013
Wael Ahmed <waelahmed6599@gmail.com>
2021-10-14 10:55:55 +0200

    Removing select from / get

--- sheet.js
+++ sheet.js
@@ -24,7 +24,7 @@ sheetRouter.post("/", async (req, res) => {
 
 sheetRouter.get("/", async (req, res) => {
   try {
-    const result = await Sheet.find().select("name -difficulty -author");
+    const result = await Sheet.find();
     res.status(200).json(createResponse("success", result));
   } catch (ex) {
     res.status(400).json(createResponse("error", x.message));
